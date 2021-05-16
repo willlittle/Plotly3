4b7f08b0a4b54c4b3c07a9ca882095132992eafa
willlittle <will.little.wl@protonmail.com>
2021-05-17 09:19:34 +1000

    my commit

--- src/App.js
+++ src/App.js
@@ -1,68 +1,78 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { Tabs, Tab, AppBar } from '@material-ui/core';
-import Chart from './Chart'
 import * as d3 from 'd3'
+import Chart from './Chart'
 
-const iState = () => {
-  let year = []
-  let mortality = []
-  let plot = []
-    d3.csv('/mortality2.csv', (d)=> {
-      if(d.index == 1){
-        year.push(d.year)
-        mortality.push(d.recordedRate)
-        plot.push(d.country)
-      }})
-    const initialState = {year, mortality, plot}
-    return initialState
-}
 
-function dataSwitcher(tabIndex){
-  let year = []
-  let mortality = []
-  let plot = []
-    d3.csv('/mortality2.csv', (d)=> {
-      if(d.index == tabIndex){
-        year.push(d.year)
-        mortality.push(d.recordedRate) 
-        plot.push(d.country)
-      }})
-  const newPlot = {year, mortality, plot}
-  return newPlot
 
-}  
+
 function App() {
 
-
-  const[currentData, setCurrentData] = useState(iState());
+  const[currentData, setCurrentData] = useState(false);
   const[tabValue, setTabValue]= useState(0)
-  const handleTabs=(e, val)=>(setTabValue(val), dataSwitcher(val))
+  const handleTabs=(e, val) => {setTabValue(val);createPlotData(val);}
 
-  const updateData = (tabIndex) =>{
-    setCurrentData(dataSwitcher(tabIndex))
+  useEffect(() => {
+    createPlotData(0)
+  }, []);
 
-  }
+  async function createPlotData (tabIndex){
+    let year = []
+    let country = []
+    let mortality = []
+    let projectedWithoutWar = []
+    let projectedFromRecord = []
+    let miscData = []
+    
+      await d3.csv('/mortality4.csv', (d)=> {
+        if(d.index == tabIndex){
+          year.push(d.year)
+          country.push(d.country)
+          mortality.push(d.recordedRate) 
+          projectedWithoutWar.push(d.projectedWithoutWar)
+          projectedFromRecord.push(d.projectedFromRecord)
+          
+          miscData.push(d.misc)
+        }})
 
-// console.log(currentData.mortality)
+    setCurrentData({
+      year: year,
+      country:country[0],
+      mortality:mortality,
+      projectedWithoutWar:projectedWithoutWar,
+      projectedFromRecord:projectedFromRecord,
+      height:mortality[0],
+      yearsAdded:miscData[0],
+      start:miscData[1],
+      end:miscData[2],
+      bio:miscData[3],
+      bio2:miscData[4],
+      bio3:miscData[5],
+      conflictName:miscData[6]
+
+    })
+
+
+  }  
+
     return (
-    <div className="App">
-      <div className='container'>
-          <div>
-            <AppBar position='static' >
-              <Tabs centered align={'center'}value={tabValue} onChange={handleTabs}>
-                <Tab label='Plot 1'/>
-                <Tab label='Plot 2'/>
-                <Tab label='Plot 3'/>
-              </Tabs>
-            </AppBar>
-          </div>
-          <div>
-            <Chart plotData={currentData} />
-          </div>
-        </div> 
+    <div className='container'>
+        <div>
+            <Tabs variant='scrollable'  centered align={'center'} value={tabValue} onChange={handleTabs}>
+              <Tab style={{maxWidth:10, color:'rgb(250,0,0)'}}label='The Idea'/>
+              <Tab label='Rwanda' />
+              <Tab label='Syria'/>
+              <Tab label='Afghanistan'/>
+              <Tab label='Bosnia and Herzegovina'/>
+              <Tab label='Iraq'/>
+            </Tabs>
+        </div>
+        <div >
+            <Chart plotData={currentData}/>
+        </div>
     </div>
-  );
+  )
 }
 
 export default App;
